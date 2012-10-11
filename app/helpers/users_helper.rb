6925d2f812a9267cef450841efaa82e4bb5be91d
Andrew Porter <partydrone@me.com>
2012-10-10 23:41:39 -0600

    Make Gravatars secure

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,6 @@ module UsersHelper
   def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    image_tag "http://gravatar.com/avatar/#{gravatar_id}?s=#{size}", alt: user.name, class: "gravatar"
+    image_tag "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}", alt: user.name, class: "gravatar"
   end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -3,6 +3,6 @@ require 'minitest_helper'
 describe UsersHelper do
   it "returns a Gravatar image tag" do
     user = User.new(name: "Barney Rubble", email: "BRubble@example.com")
-    gravatar_for(user).must_equal '<img alt="Barney Rubble" class="gravatar" src="http://gravatar.com/avatar/55dadad828006ab56c23b8e87bdf8aee?s=50" />'
+    gravatar_for(user).must_equal '<img alt="Barney Rubble" class="gravatar" src="https://secure.gravatar.com/avatar/55dadad828006ab56c23b8e87bdf8aee?s=50" />'
   end
 end
